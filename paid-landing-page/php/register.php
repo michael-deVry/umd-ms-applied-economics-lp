2bc65b94de41a32705ccd131224ef9c49f43f123
Michael deVry <57021865+michael-deVry@users.noreply.github.com>
2023-01-25 13:15:40 -0500

    Add files via upload
    Updates to field variables

--- paid-landing-page/php/register.php
+++ paid-landing-page/php/register.php
@@ -1,31 +1,31 @@
 <?php
 
-$field_first_name = $_POST['first'];
-$field_last_nanme = $_POST['last'];
-$field_email = $_POST['email'];
-$field_semester = $_POST['semester'];
-$field_visa = $_POST['visa'];
-$field_message = $_POST['message'];
+$first = $_POST['first'];
+$last = $_POST['last'];
+$email = $_POST['email'];
+$semester = $_POST['semester'];
+$visa = $_POST['visa'];
+$message = $_POST['message'];
 
 $mail_to = 'masters-econ@umd.edu';
 
-$subject = 'MS Applied Economics Information Request'.$field_first;
+$subject = 'MS Applied Economics Information Request'.$first;
 
-$body_message = 'Potential Applicant First Name: '.$field_first."\n";
+$body_message = 'Potential Applicant First Name: '.$first."\n";
 
-$body_message = 'Potential Applicant Last Name: '.$field_last."\n";
+$body_message = 'Potential Applicant Last Name: '.$last."\n";
 
-$body_message .= 'Potential Applicant E-mail: '.$field_email."\n";
+$body_message .= 'Potential Applicant E-mail: '.$email."\n";
 
-$body_message .= 'Desired Program Semester: '.$field_semester."\n";
+$body_message .= 'Desired Program Semester: '.$semester."\n";
 
-$body_message .= 'F1 Visa Sponsorship Needed: '.$field_visa."\n";
+$body_message .= 'F1 Visa Sponsorship Needed: '.$visa."\n";
 
-$body_message .= 'Message and Questions: '.$field_message."\n";
+$body_message .= 'Message and Questions: '.$message."\n";
 
-$headers = 'From: '.$field_email."\r\n";
+$headers = 'From: '.$email."\r\n";
 
-$headers .= 'Reply-To: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$email."\r\n";
 
 $mail_status = mail($mail_to, $subject, $body_message, $headers);
 
